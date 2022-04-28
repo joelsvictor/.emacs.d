c97252f90fa706607d40ea85229abfd9fd53511e
Joel Victor <joelsvictor@gmail.com>
2022-04-28 08:39:11 +0530

    refactor: move clojure packages into a separate file

---
+++ clojure.el
@@ -0,0 +1,46 @@
+;;; package --- clojure.el
+;;; Commentary:
+;;; Code:
+
+(require 'use-package)
+
+
+(use-package cider
+  :straight t
+  :ensure t
+  :custom
+  (cider-prompt-for-symbol nil)
+  (cider-repl-pop-to-buffer-on-connect nil)
+  (cider-repl-prompt-function 'cider-repl-prompt-abbreviated)
+  (cider-repl-buffer-size-limit 100000)
+  (nrepl-log-messages t)
+  (cider-auto-test-mode t)
+  :bind (:map clojure-mode-map
+              ("C-c c d d" . cider-debug-defun-at-point))
+  :commands cider-debug-defun-at-point)
+
+(use-package paredit
+  :straight t
+  :ensure t
+  :hook ((emacs-lisp-mode lisp-mode clojure-mode cider-repl-mode) . paredit-mode))
+
+
+(use-package flycheck-clj-kondo
+  :straight t
+  :ensure-system-package (clj-kondo  . "brew install borkdude/brew/clj-kondo"))
+
+
+(use-package cljstyle-mode
+  :straight (:host github :repo "jstokes/cljstyle-mode" :branch "master")
+  :hook ((clojure-mode) . cljstyle-mode)
+  :ensure-system-package (cljstyle . "brew install --cask cljstyle"))
+
+
+(use-package clj-refactor
+  :straight t
+  :ensure t
+  :hook (clojure-mode . clj-refactor-mode))
+
+
+(provide 'clojure)
+;;; clojure.el ends here
--- init.el
+++ init.el
@@ -64,19 +64,20 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(require 'straight)
 
 (straight-use-package 'use-package)
-
 (straight-use-package 'use-package-ensure-system-package)
 
 (require 'use-package)
 
-(setq use-package-verbose t)
+;; (setq use-package-verbose t)
 
 
 (use-package exec-path-from-shell
-  :straight (:type git :host github :repo "purcell/exec-path-from-shell" :branch "master")
+  :straight (:type git
+                   :host github
+                   :repo "purcell/exec-path-from-shell"
+                   :branch "master")
   :ensure t)
 
 
@@ -102,46 +103,6 @@
 ;;   (define-key evil-ex-map "f " 'find-file)
 ;;   (define-key evil-ex-map "b " 'ibuffer))
 
-
-(use-package paredit
-  :straight t
-  :ensure t
-  :hook ((emacs-lisp-mode lisp-mode) . paredit-mode))
-
-
-(use-package cider
-  :straight t
-  :ensure t
-  :hook ((clojure-mode cider-repl-mode) . paredit-mode)
-  :custom
-  (cider-prompt-for-symbol nil)
-  (cider-repl-pop-to-buffer-on-connect nil)
-  (cider-repl-prompt-function 'cider-repl-prompt-abbreviated)
-  (cider-repl-buffer-size-limit 100000)
-  (nrepl-log-messages t)
-  (cider-auto-test-mode t)
-  :bind (:map clojure-mode-map
-              ("C-c c d d" . cider-debug-defun-at-point))
-  :commands cider-debug-defun-at-point)
-
-
-(use-package flycheck-clj-kondo
-  :straight t
-  :ensure-system-package (clj-kondo  . "brew install borkdude/brew/clj-kondo"))
-
-
-(use-package cljstyle-mode
-  :straight (:host github :repo "jstokes/cljstyle-mode" :branch "master")
-  :hook ((clojure-mode) . cljstyle-mode)
-  :ensure-system-package (cljstyle . "brew install --cask cljstyle"))
-
-
-(use-package clj-refactor
-  :straight t
-  :ensure t
-  :hook (clojure-mode . clj-refactor-mode))
-
-
 (use-package flycheck
   :straight t
   :ensure t
@@ -152,7 +113,17 @@
   (global-flycheck-mode))
 
 
+(require 'clojure (expand-file-name "clojure.el" user-emacs-directory))
+
+
+(use-package paredit
+  :straight t
+  :ensure t
+  :hook ((emacs-lisp-mode lisp-mode clojure-mode cider-repl-mode) . paredit-mode))
+
+
 (use-package lsp-mode
+  :disabled
   :straight t
   :ensure t
   :init (setq lsp-keymap-prefix "C-c l")
@@ -167,6 +138,7 @@
 
 
 (use-package lsp-ui
+  :disabled
   :straight t
   :ensure t
   :hook ((lsp-mode) . lsp-ui-mode)
@@ -177,6 +149,7 @@
 
 
 (use-package lsp-treemacs
+  :disabled
   :straight t
   :ensure t
   :custom
@@ -228,6 +201,7 @@
 
 (use-package embark
   :straight t
+  :disabled
   :ensure t
   :bind (("C-." . embark-act)))
 
@@ -279,58 +253,6 @@
      (plantuml . t)
      (sql . t)
      (verb . t))))
-
-
-;; (use-package org-bullets
-;;   :straight t
-;;   :config
-;;   (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-
-;; (let* ((variable-tuple
-;;         (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
-;;               ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-;;               ((x-list-fonts "JetBrains Mono")         '(:font "JetBrains Mono"))
-;;               ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-;;               ((x-list-fonts "Verdana")         '(:font "Verdana"))
-;;               ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-;;               (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-;;        (base-font-color     (face-foreground 'default nil 'default))
-;;        (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
-
-;;   (custom-theme-set-faces
-;;    'user
-;;    `(org-level-8 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-7 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-6 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-5 ((t (,@headline ,@variable-tuple))))
-;;    `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-;;    `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-;;    `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-;;    `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-;;    `(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
-
-;; (custom-theme-set-faces
-;;    'user
-;;    '(variable-pitch ((t (:family "JetBrains Mono" :height 180 :weight thin))))
-;;    '(fixed-pitch ((t ( :family "Ayuthaya" :height 160)))))
-
-;; (add-hook 'org-mode-hook 'variable-pitch-mode)
-
-;; (custom-theme-set-faces
-;;    'user
-;;    '(org-block ((t (:inherit fixed-pitch))))
-;;    '(org-code ((t (:inherit (shadow fixed-pitch)))))
-;;    '(org-document-info ((t (:foreground "dark orange"))))
-;;    '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
-;;    '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
-;;    '(org-link ((t (:foreground "royal blue" :underline t))))
-;;    '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-;;    '(org-property-value ((t (:inherit fixed-pitch))) t)
-;;    '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
-;;    '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
-;;    '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
-;;    '(org-verbatim ((t (:inherit (shadow fixed-pitch))))))
 
 
 (use-package lsp-haskell
@@ -391,7 +313,6 @@
   :ensure t)
 
 
-
 (use-package dockerfile-mode
   :straight t
   :ensure t)
@@ -427,7 +348,9 @@
 
 (use-package anzu
   :straight t
-  :ensure t)
+  :ensure t
+  :config
+  (global-anzu-mode))
 
 
 (use-package auto-virtualenv
@@ -484,6 +407,17 @@
 ;;     (eaf-bind-key nil "M-q" eaf-browser-keybinding))
 ;;   )
 ;; unbind, see more in the Wiki
+
+
+(use-package tree-sitter
+  :straight t
+  :config
+  (global-tree-sitter-mode))
+
+(use-package tree-sitter-langs
+  :straight t)
+
+
 
 (server-start)
 
