6819ae0321d1ce04d4a64c99b972b17c89db052a
Joel Victor <joelsvictor@gmail.com>
2022-04-30 15:33:31 +0530

    setup a simple modeline

--- early-init.el
+++ early-init.el
@@ -7,5 +7,14 @@
 (setq read-process-output-max 16777216)
 (add-to-list 'default-frame-alist
              '(font . "JetBrains Mono-14"))
-
+(setq-default mode-line-format
+              '("%e"
+                (:propertize
+                 ("" mode-line-mule-info mode-line-client mode-line-modified mode-line-remote)
+                 display
+                 (min-width
+                  (5.0)))
+                mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position (vc-mode vc-mode)
+                mode-line-misc-info
+                " [%I]"))
 ;;; early-init.el ends here
