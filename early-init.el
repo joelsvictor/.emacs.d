c52a3ccab5dc49bbd96d26136d29da7083b4365b
Joel Victor <joelsvictor@gmail.com>
2022-05-01 11:40:50 +0530

    modeline needs improvement since it doesn't show important information

--- early-init.el
+++ early-init.el
@@ -18,14 +18,14 @@
 
 (setq visible-bell t)
 
-(setq-default mode-line-format
-              '("%e"
-                (:propertize
-                 ("" mode-line-mule-info mode-line-client mode-line-modified mode-line-remote)
-                 display
-                 (min-width
-                  (5.0)))
-                mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position (vc-mode vc-mode)
-                mode-line-misc-info
-                " [%I]"))
+;;(setq-default mode-line-format
+;;              '("%e"
+;;                (:propertize
+;;                 ("" mode-line-mule-info mode-line-client mode-line-modified mode-line-remote)
+;;                 display
+;;                 (min-width
+;;                  (5.0)))
+;;                mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position (vc-mode vc-mode)
+;;                mode-line-misc-info
+;;                " [%I]"))
 ;;; early-init.el ends here
