89f04a26b89a196c3b9b06b4c55d28ee67338d47
Joel Victor <joelsvictor@gmail.com>
2022-10-17 22:27:08 +0530

    fix: erase all customizations

--- custom.el
+++ custom.el
@@ -10,6 +10,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-block ((t (:extend t :background "#191F26" :foreground "#d4d4d6" :height 1.2 :family "JetBrains Mono"))))
- '(org-code ((t (:foreground "#cd9575" :height 1.5 :family "Fira Code"))))
- '(org-level-1 ((t (:foreground "#0D9C94" :weight bold :height 1.7 :family "Monaco")))))
+)
